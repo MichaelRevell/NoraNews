5557ce6d3ebf1ab4fb7342e4360d122462ab06bd
Michael Revell <mikearevell@gmail.com>
2013-06-21 22:13:00 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Noranews::Application.config.secret_token = '4baba1860d96166c7228083a2b8d82b97006fd6ba51b5ab8377c92ec8fbefec5e55ca97216e36e80d168e6146878555889e8633579f40a49dac5f2c21d77c6e0'
