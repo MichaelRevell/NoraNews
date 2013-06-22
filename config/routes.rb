7e8cc09b11c6b58f448306c00346484afa853d64
Michael Revell <mikearevell@gmail.com>
2013-06-21 23:06:59 -0700

    devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Noranews::Application.routes.draw do
 
-  root to: 'main#home'
+  devise_for :users, :path => '', :path_names => { :sign_in => 'login', :sign_out => 'logout', :password => 'secret', :confirmation => 'verification', :unlock => 'unblock', :registration => 'register', :sign_up => 'signup' }
+
+  get "home/index"
+
+  root :to => "home#index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
